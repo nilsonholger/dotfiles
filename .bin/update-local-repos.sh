be8d14d5a2b623e98891ed9a44dc7120c4c7e648
nilsonholger <nilsonholger@hyve.org>
2012-04-08 15:31:05 +0200

    BIN small changes in update script

--- .bin/update-local-repos.sh
+++ .bin/update-local-repos.sh
@@ -2,45 +2,44 @@
 
 _SRCDIR="$HOME/local"
 
-_CVS=''
-_HAS_UPDATES=''
-_LOG="$_SRCDIR/update-local-repos.log"
-_PWD_ORIG=$PWD
+_SCM=''
+_LOG="$_SRCDIR/update.log"
+_UPDATE=''
 
 function update_git {
-_CVS='GIT'
+_SCM='       GIT'
 _STATUS=`git pull`
-[ $_STATUS = "Already up-to-date." ] && _HAS_UPDATES='' || _HAS_UPDATES=true
+[ $_STATUS = "Already up-to-date." ] && _UPDATE='' || _UPDATE=true
 }
 
 function update_mercurial {
-_CVS='MERCURIAL'
+_SCM=' MERCURIAL'
 _STATUS=`hg pull`
-[ ${_STATUS/*no changes found}x = "x" ] && _HAS_UPDATES='' || _HAS_UPDATES=true
+[ ${_STATUS/*no changes found}x = "x" ] && _UPDATE='' || _UPDATE=true
 }
 
 function update_subversion {
-_CVS='SUBVERSION'
+_SCM='SUBVERSION'
 _STATUS=`svn update`
-[ ${_STATUS/At revision *.}x = "x" ] && _HAS_UPDATES='' || _HAS_UPDATES=true
+[ ${_STATUS/At revision *.}x = "x" ] && _UPDATE='' || _UPDATE=true
 }
 
 echo "`date`" >> $_LOG
-echo "UPDATING LOCAL REPOSITORIES IN $_SRCDIR" | tee -a $_LOG
+echo "[ DIRECTORY]>>> $_SRCDIR" | tee -a $_LOG
 for _DIR in $_SRCDIR/*
 do
     [ -d $_DIR ] || continue
     cd $_DIR
     _DIR=`basename $_DIR`
-    _CVS=''
+    _SCM=''
     [ -d .git ] && update_git
     [ -d .hg ] && update_mercurial
     [ -d .svn ] && update_subversion
-    [ ! $_CVS ] && (echo "---SKIPPED $_DIR [NO_CVS_DVS]" | tee -a $_LOG) && continue
-    [ ! $_HAS_UPDATES ] && (echo "===CURRENT $_DIR [$_CVS]" | tee -a $_LOG) && continue
+    [ ! $_SCM ] && (echo "[  SKIPPING]--- $_DIR" | tee -a $_LOG) && continue
+    [ ! $_UPDATE ] && (echo "[$_SCM]=== $_DIR" | tee -a $_LOG) && continue
+    echo >> $_LOG
+    echo "[$_SCM]+++ $_DIR" | tee -a $_LOG
     make >> $_LOG
     make install >> $_LOG
-    echo "+++UPDATED $_DIR [$_CVS]" | tee -a $_LOG
+    echo >> $_LOG
 done
-
-cd $_PWD_ORIG
