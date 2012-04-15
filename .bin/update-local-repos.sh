9f9d059a97ebfb3f5d1c2646c299c8b089139617
nilsonholger <nilsonholger@hyve.org>
2012-04-15 23:28:38 +0200

    BIN make update functions more verbose

--- .bin/update-local-repos.sh
+++ .bin/update-local-repos.sh
@@ -7,6 +7,7 @@ _UPDATE_ORDER="$_SRCDIR/.update-order"
 _INDEX=0
 _SCM=''
 _UPDATE=''
+_TEST="$_SRCDIR/.test-output"
 
 function out_buffer {
 ((_INDEX++))
@@ -22,21 +23,24 @@ done
 }
 
 function update_git {
-_SCM='       GIT'
-_STATUS=`git pull 2>&1`
-[ $_STATUS = "Already up-to-date." ] && _UPDATE='' || _UPDATE=true
+_SCM='GIT'
+echo "GIT STATUS" | add_prefix $1
+git pull 2>&1 | tee $_TEST | add_prefix $1
+grep -q -E "(Already up-to-date|Current branch .* is up to date)" $_TEST && _UPDATE='' || _UPDATE=true
 }
 
 function update_mercurial {
-_SCM=' MERCURIAL'
-_STATUS=`hg pull`
-[ ${_STATUS/*no changes found}x = "x" ] && _UPDATE='' || _UPDATE=true
+_SCM='MERCURIAL'
+echo "MERCURIAL STATUS" | add_prefix $1
+hg pull | tee $_TEST | add_prefix $1
+grep -q "no changes found" $_TEST && _UPDATE='' || _UPDATE=true
 }
 
 function update_subversion {
 _SCM='SUBVERSION'
-_STATUS=`svn update`
-[ ${_STATUS/At revision *.}x = "x" ] && _UPDATE='' || _UPDATE=true
+echo "SUBVERSION STATUS" | add_prefix $1
+svn update | tee $_TEST | add_prefix $1
+grep -q "At revision" $_TEST && _UPDATE='' || _UPDATE=true
 }
 
 echo "`date`" > $_LOG
@@ -46,10 +50,10 @@ do
     [ -d $_DIR ] || continue
     cd $_DIR
     _SCM=''
-    echo -n " $_DIR"
-    [ -d .git ] && update_git
-    [ -d .hg ] && update_mercurial
-    [ -d .svn ] && update_subversion
+    [ -d .git ] && update_git $_DIR
+    [ -d .hg ] && update_mercurial $_DIR
+    [ -d .svn ] && update_subversion $_DIR
+    [ -d CVS ] && update_cvs $_DIR
     [ ! $_SCM ] && out_buffer "--- $_DIR [NO_SCM]" && continue
     [ ! $_UPDATE ] && out_buffer "=== $_DIR [$_SCM]" && continue
     out_buffer "+++ $_DIR [$_SCM]"
