cbf5150a6da319e420dc1f536931658b2d89c34c
nilsonholger <nilsonholger@hyve.org>
2012-04-21 16:03:09 +0200

    BIN due to request, licensing added

--- .bin/osx.sh
+++ .bin/osx.sh
@@ -5,6 +5,26 @@
 # https://github.com/mathiasbynens/dotfiles
 # http://www.nsa.gov/ia/_files/factsheets/macosx_10_6_hardeningtips.pdf
 # http://www.nsa.gov/applications/links/notices.cfm?address=http://images.apple.com/support/security/guides/docs/SnowLeopard_Security_Config_v10.6.pdf
+#
+# Copyright (C) 2012 nilsonholger@hyve.org
+#
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the "Software"), to deal
+# in the Software without restriction, including without limitation the rights
+# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions:
+#
+# The above copyright notice and this permission notice shall be included in
+# all copies or substantial portions of the Software.
+#
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+# THE SOFTWARE.
 
 function disk {
 # turn of sudden motion sensor
