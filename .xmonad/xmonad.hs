6c5bd4f4ef56e0d8f3d8b5b678de9d4ad7490a8c
nilsonholger <nilsonholger@hyve.org>
2013-12-03 11:19:54 +0100

    xmonad: welcome triple monitor setup, fix layout, reformat

--- .xmonad/xmonad.hs
+++ .xmonad/xmonad.hs
@@ -8,34 +8,35 @@ import XMonad.Util.EZConfig (additionalKeys)
 import qualified XMonad.StackSet as W
 import qualified Data.Map        as M
 
-spaces         = ["main","ext","tmp","tmp2"]
+spaces = ["main","ext","tmp","tmp2"]
 
 main = xmonad $ gnomeConfig
     {
-		borderWidth        = 1,
-		normalBorderColor  = "#002200",
-		focusedBorderColor = "#005500",
-		workspaces         = spaces,
-		modMask            = mod4Mask,
-		manageHook = composeAll
-			[ manageHook gnomeConfig
-			, className =? "MPlayer"            --> doFloat
-			, className =? "Gimp"               --> doFloat
-			, resource  =? "desktop_window"     --> doIgnore ],
-		startupHook        = do
-			setWMName "LG3D"
+        modMask = mod4Mask,
+        borderWidth = 1,
+        normalBorderColor = "#002200",
+        focusedBorderColor = "#005500",
+        workspaces = spaces,
+        layoutHook = Full ||| Tall 1 (3/100) (1/2) ||| Mirror (Tall 1 (3/100) (1/2)),
+        manageHook = composeAll
+            [ manageHook gnomeConfig
+            , className =? "MPlayer" --> doFloat
+            , className =? "Gimp" --> doFloat
+            , resource  =? "desktop_window" --> doIgnore ],
+        startupHook = do
+            setWMName "LG3D"
     }
-	`additionalKeys`
-	[((m .|. mod4Mask, k), windows $ f i)
-		| (i, k) <- zip (spaces) [xK_ampersand, xK_bracketleft, xK_braceleft, xK_braceright]
-		, (f, m) <- [(W.greedyView, 0), (W.shift, shiftMask)]]
-	`additionalKeys`
-	[((m .|. mod4Mask, k), screenWorkspace s >>= flip whenJust (windows . f))
-		 | (k, s) <- zip [xK_w, xK_e] [0,1]
-		, (f, m) <- [(W.view, 0), (W.shift, shiftMask)]]
-	`additionalKeys` [
-	((mod4Mask              , xK_q     ), spawn "xmonad --recompile; xmonad --restart&"),
-	((mod4Mask              , xK_Escape), spawn "display ~/.xmonad/Xmbindings.png &"),
-	((mod4Mask .|. shiftMask, xK_i     ), spawn "firefox &"),
-	((mod4Mask .|. shiftMask, xK_m     ), spawn "thunderbird &"),
-	((mod4Mask .|. shiftMask, xK_l     ), spawn "if [ \"`setxkbmap -print | grep -o dvp`\" = \"dvp\" ]; then setxkbmap -layout us_intl -option 'grp:shifts_toggle'; else setxkbmap -layout us,us,de -variant dvp,,nodeadkeys -option lv3:ralt_switch; fi; xmodmap ~/.Xmodmap &")]
+    `additionalKeys`
+    [((m .|. mod4Mask, k), windows $ f i)
+        | (i, k) <- zip (spaces) [xK_ampersand, xK_bracketleft, xK_braceleft, xK_braceright]
+        , (f, m) <- [(W.greedyView, 0), (W.shift, shiftMask)]]
+    `additionalKeys`
+    [((m .|. mod4Mask, k), screenWorkspace s >>= flip whenJust (windows . f))
+        | (k, s) <- zip [xK_w, xK_e, xK_r] [1,2,0]
+        , (f, m) <- [(W.view, 0), (W.shift, shiftMask)]]
+    `additionalKeys` [
+        ((mod4Mask              , xK_q     ), spawn "xmonad --recompile && xmonad --restart&"),
+        ((mod4Mask              , xK_Escape), spawn "display ~/.xmonad/Xmbindings.png &"),
+        ((mod4Mask .|. shiftMask, xK_i     ), spawn "firefox &"),
+        ((mod4Mask .|. shiftMask, xK_m     ), spawn "thunderbird &"),
+        ((mod4Mask .|. shiftMask, xK_l     ), spawn "if [ \"`setxkbmap -print | grep -o dvp`\" = \"dvp\" ]; then setxkbmap -layout us_intl -option 'grp:shifts_toggle'; else setxkbmap -layout us,us,de -variant dvp,,nodeadkeys -option lv3:ralt_switch; fi; xmodmap ~/.Xmodmap &")]
