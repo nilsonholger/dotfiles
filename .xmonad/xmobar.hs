78acdaf1d348617d41fb6c16a5943e011cd3ac13
nilsonholger <nilsonholger@hyve.org>
2014-02-06 20:51:10 +0100

    xmonad: add units to xmobar's bandwidth

--- .xmonad/xmobar.hs
+++ .xmonad/xmobar.hs
@@ -5,7 +5,7 @@ Config {
 		Run StdinReader
 		, Run MultiCpu ["-L","20","-H","90","-n","green","-h","red","-t","<autobar>"] 10
 		, Run Com "/sbin/ifconfig" ["wlan0","|","awk","'/ssid/{print $2}'"] "ssid" 100
-		, Run Com "netstat" ["-h","-w1","-q1","|","awk","'NR>2 {print $4\\"|\\" $7}'"] "net" 10
+		, Run Com "netstat" ["-h","-w1","-q1","|","awk","'NR>2 {print $4\\"B|\\" $7\\"B\\"}'"] "net" 10
 		, Run Com "vmstat" ["-h","|","awk","'NR>2 {print $5}'"] "mem" 50
 		, Run Com "/sbin/zpool" ["list","|","awk","'/zroot/{print $4}'"] "zpool" 100
 		, Run Kbd [("us(dvp)", "DV"), ("us(alt-intl)", "US")]
