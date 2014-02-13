c5b4960b6f414513bb26b120de16ef56388c989a
nilsonholger <nilsonholger@hyve.org>
2014-02-13 18:09:53 +0100

    xmonad: add thermal zone info to xmobar

--- .xmonad/xmobar.hs
+++ .xmonad/xmobar.hs
@@ -8,10 +8,11 @@ Config {
 		, Run Com "netstat" ["-h","-w1","-q1","|","awk","'NR>2 {print $4\\"B|\\" $7\\"B\\"}'"] "net" 10
 		, Run Com "vmstat" ["-h","|","awk","'NR>2 {print $5}'"] "mem" 50
 		, Run Com "/sbin/zpool" ["list","|","awk","'/zroot/{print $4}'"] "zpool" 100
+		, Run Com "/sbin/sysctl" ["-n","hw.acpi.thermal.tz0.temperature"] "tz" 100
 		, Run Kbd [("us(dvp)", "DV"), ("us(alt-intl)", "US")]
 		, Run Date "%Y-%m-%d %H:%M" "date" 10
 	]
 		, sepChar = "%"
 		, alignSep = "}{"
-		, template = "%StdinReader%}<fc=#333333><action=urxvt -e htop>%multicpu%</action></fc>{<fc=#765400><%net%></fc> <fc=#987600><action=wifimgr>%ssid%</action></fc> <fc=#ba9800>m:%mem% z:%zpool%</fc> <fc=#cb6500>%kbd%</fc> <fc=#fe3200><action=xclock>%date%</action></fc>"
+		, template = "%StdinReader%}<fc=#333333><action=urxvt -e htop>%multicpu%</action></fc>{<fc=#765400><%net%></fc> <fc=#987600><action=wifimgr>%ssid%</action></fc> <fc=#ba9800>m:%mem% z:%zpool% %tz%</fc> <fc=#cb6500>%kbd%</fc> <fc=#fe3200><action=xclock>%date%</action></fc>"
 }
