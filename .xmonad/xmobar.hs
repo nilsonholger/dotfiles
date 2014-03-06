8251d28ee8b17fd5cab628cfc8bee5cf366ca927
nilsonholger <nilsonholger@hyve.org>
2014-03-06 11:11:50 +0100

    xmonad: add battery and some actions to xmobar

--- .xmonad/xmobar.hs
+++ .xmonad/xmobar.hs
@@ -9,10 +9,11 @@ Config {
 		, Run Com "vmstat" ["-h","|","awk","'NR>2 {print $5}'"] "mem" 50
 		, Run Com "/sbin/zpool" ["list","|","awk","'/zroot/{print $4}'"] "zpool" 100
 		, Run Com "/sbin/sysctl" ["-n","hw.acpi.thermal.tz0.temperature"] "tz" 100
+		, Run Com "/sbin/sysctl" ["-n","hw.acpi.battery.life"] "bat" 100
 		, Run Kbd [("us(dvp)", "DV"), ("us(alt-intl)", "US")]
 		, Run Date "%Y-%m-%d %H:%M" "date" 10
 	]
-		, sepChar = "%"
+		, sepChar = "$"
 		, alignSep = "}{"
-		, template = "%StdinReader%}<fc=#333333><action=urxvt -e htop>%multicpu%</action></fc>{<fc=#765400><%net%></fc> <fc=#987600><action=wifimgr>%ssid%</action></fc> <fc=#ba9800>m:%mem% z:%zpool% %tz%</fc> <fc=#cb6500>%kbd%</fc> <fc=#fe3200><action=xclock>%date%</action></fc>"
+		, template = "$StdinReader$}<fc=#333333><action=urxvt -e htop>$multicpu$</action></fc>{<fc=#765400><action=urxvt -e systat -netstat><$net$></action></fc> <fc=#987600><action=wifimgr>$ssid$</action></fc> <fc=#ba9800><action=urxvt -e systat -vmstat>m:$mem$</action> <action=urxvt -e systat -iostat>z:$zpool$</action> $tz$ $bat$%</fc> <fc=#cb6500>$kbd$</fc> <fc=#fe3200><action=xclock>$date$</action></fc>"
 }
