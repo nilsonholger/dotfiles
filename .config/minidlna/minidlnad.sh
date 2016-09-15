57c098cbc95155fbf9e1ce9585f4e6307ad0c2a7
nilsonholger <nilsonholger@hyve.org>
2016-09-15 13:06:02 +0200

    minidlna: fix db rebuild issues

--- .config/minidlna/minidlnad.sh
+++ .config/minidlna/minidlnad.sh
@@ -1,12 +1,19 @@
 #! /bin/zsh
 _CONFIG="$HOME/.config/minidlna/minidlna.conf"
 _PID="$HOME/.config/minidlna/minidlnad.pid"
+_DB="$HOME/.config/minidlna/cache/files.db"
 
+function _main() {
 case $1 in
+	reload) rm -f ${_DB}; _main stop; _main start;;
 	stop) killall minidlnad;;
-	start|reload|*)
-		[ -f "${_PID}" ] && $0 stop
-		minidlnad -v -f "${_CONFIG}" -P "${_PID}" -R;;
+	start|*)
+		[ -f "${_PID}" ] && _main stop
+		minidlnad -v -f "${_CONFIG}" -P "${_PID}" -R
+		;;
 esac
+}
+
+_main $*
 
 tail -n 5 minidlna.log
