74fb0ea5f1de6dbc8b4323759e76ce9f44df6ea7
nilsonholger <nilsonholger@hyve.org>
2018-10-18 14:03:34 +0200

    xmonad: adapt to xmonad's ManageDock changes for xmobar

--- .xmonad/xmobar.hs
+++ .xmonad/xmobar.hs
@@ -1,7 +1,9 @@
 Config {
 	  font = "-misc-fixed-*-*-*-*-10-*-*-*-*-*-*-*"
-	, lowerOnStart = True
-	, overrideRedirect = False
+	--, hideOnStart = True
+	--, lowerOnStart = True
+	--, overrideRedirect = True
+	--, persistent = True
 	, commands = [
 		Run StdinReader
 		, Run MultiCpu ["-L","60","-H","90","-n","yellow","-h","red","-t","<autobar>"] 10
